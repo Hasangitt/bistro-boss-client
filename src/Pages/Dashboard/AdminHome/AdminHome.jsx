ad297c3125b2d9368b7e6765ff30545fe89fd931
Hasangitt <pingpong038759@gmail.com>
2025-01-25 00:09:40 +0600

    commit

--- src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -3,108 +3,65 @@ import { AuthContext } from "../../Auth/Providers/AuthContext";
 import { useQuery } from "@tanstack/react-query";
 import useAxios from "../../../hooks/useAxios/useAxios";
 
+import { FaMoneyBill, FaUsers } from "react-icons/fa";
+
+import { PiChefHatThin } from "react-icons/pi";
+import { GrDeliver } from "react-icons/gr";
+
 const AdminHome = () => {
   const { user } = useContext(AuthContext);
   const axiosSecure = useAxios();
-  const { data } = useQuery({
+  const { data = {}} = useQuery({
     queryKey: ["admin-stats"],
     queryFn: async () => {
       const res = await axiosSecure.get("/admin-stats");
-      console.log(data);
-      return res.data;
-    },
+      console.log(data)
+      return res.data
+    }
   });
   return (
-    <div>
+    <div className="flex flex-col mx-8 my-4 space-y-4">
       <div>
         <h1 className="text-3xl text-black font-bold">
           <span>Hi, Welcome</span>{" "}
           <span>{user?.displayName ? user?.displayName : "Back"}</span>
         </h1>
       </div>
-      <div>
-        <div className="stats shadow">
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-8 w-8 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
-                ></path>
-              </svg>
-            </div>
-            <div className="stat-title"></div>
+      <div className="stats border shadow-md mt-2 ">
+        <div className="flex stat bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+          <div className="stat-figure text-3xl text-white">
+            <FaMoneyBill />
+          </div>
+          <div className="text-white">
             <div className="stat-value">${data.revenue}</div>
-            <div className="stat-desc">↗︎ 400 (22%)</div>
+            <div className="">Revenue</div>
           </div>
-
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-8 w-8 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
-                ></path>
-              </svg>
-            </div>
-            <div className="stat-title">New Registers</div>
+        </div>
+        <div className="flex stat bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+          <div className="stat-figure text-3xl text-white">
+            <FaUsers />
+          </div>
+          <div className="text-white">
             <div className="stat-value">{data.users}</div>
-            <div className="stat-desc">↘︎ 90 (14%)</div>
+            <div className="">Customers</div>
           </div>
-
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-8 w-8 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
-                ></path>
-              </svg>
-            </div>
-            <div className="stat-title">New Registers</div>
-            <div className="stat-value">{data.orders}</div>
-            <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+        <div className="flex stat bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+          <div className="stat-figure text-3xl text-white">
+            <PiChefHatThin />
           </div>
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-8 w-8 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
-                ></path>
-              </svg>
-            </div>
-            <div className="stat-title">New Registers</div>
+          <div className="text-white">
             <div className="stat-value">{data.menuItems}</div>
-            <div className="stat-desc">↘︎ 90 (14%)</div>
+            <div className="">Menu Items</div>
+          </div>
+        </div>
+        <div className="flex stat bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+          <div className="stat-figure text-3xl text-white">
+            <GrDeliver />
+          </div>
+          <div className="text-white">
+            <div className="stat-value">{data.orders}</div>
+            <div className="">Orders</div>
           </div>
         </div>
       </div>
